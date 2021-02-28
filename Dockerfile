f4e782c532a3d930300f0ae12f0f2298f1a2acba
llescure <llescure@student.42.fr>
2021-02-28 16:20:51 +0100

    services installed

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,29 @@
 #Tell Docker where to start
 FROM debian:buster
 
-#Check update
+#Install the needed tools and make sure they are updated
 RUN apt-get update && apt-get -y install \
+wget \
 nginx \
 mariadb-server \
 php7.3 php-mysql php-fpm php-pdo php-gd php-cli php-mbstring
 
+#Copy the files in srcs to a file root in the docker
 COPY ./srcs root/
+
+#Set the directory where to dowload phpmyadmin and wordpress
+WORKDIR /var/www/
+
+#Download phpmyadmin
+RUN wget https://files.phpmyadmin.net/phpMyAdmin/5.1.0/phpMyAdmin-5.1.0-english.tar.gz
+RUN tar -xzvf phpMyAdmin-5.1.0-english.tar.gz
+RUN rm -rf phpMyAdmin-5.1.0-english.tar.gz
+RUN mv phpMyAdmin-5.1.0-english phpmyadmin
+
+#Download wordpress
+RUN wget https://wordpress.org/latest.tar.gz
+RUN tar -xzvf latest.tar.gz
+RUN rm -rf latest.tar.gz
+
+#Run my .sh with bash that interprets shell command
+#CMD bash /root/init_docker.sh
--- srcs/init_docker.sh
+++ srcs/init_docker.sh
@@ -1,10 +1,10 @@
 #Start the server, php and the database in the docker
-start service nginx
-start service php7.3-fpm start
-start service mysql
+service nginx start
+service php7.3-fpm start
+service mysql start
 
 #Initialize nginx : copy the new file nginx_config into the server + create a link between sites-available and sites-enabled + delete the default file
-cp root/nginx_config /etc/nginx/sites-available/nginx_config
+cp /root/nginx_config /etc/nginx/sites-available/nginx_config
 ln -s /etc/nginx/sites-available/nginx_config /etc/nginx/sites-enabled/nginx_config
 rm -rf /etc/nginx/sites-enabled/default
 rm -rf /etc/nginx/sites-available/default
@@ -12,4 +12,9 @@ rm -rf /etc/nginx/sites-available/default
 #Create a wordpress database and add a new user
 echo "CREATE DATABASE wordpress_db;" | mysql --user root
 echo "GRANT ALL ON wordpress_db.* TO 'user42'@'localhost' IDENTIFIED BY 'user42' WITH GRANT OPTION" | mysql --user root
-echo "FLUSH PRIVILEGES" | mysql --user root
+
+#Initialize phpmyadmin
+cp /root/php_config.php /var/www/phpmyadmin
+
+#Initialize wordpress
+cp /root/wordpress_config.php /var/www/wordpress
